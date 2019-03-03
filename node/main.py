c46953f4e42502b7209df88b8b4035a0b703e6dc
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-03-03 18:20:53 -0500

    Update main.py

--- node/main.py
+++ node/main.py
@@ -1,35 +1,40 @@
 import serial
 #Global Variables
 ser = 0
-
+longitude = 0
+latitude 0
+altitude = 0
 #Function to Initialize the Serial Port
 def init_serial():
-    ser = serial.Serial("/dev/cu.usbmodemPy99b4b31",115200, timeout=0) 
+    ser = serial.Serial("/dev/cu.usbmodemPy99b4b31")
+    ser.baudrate= 115200
+    ser.port= '/dev/cu.usbmodemPy99b4b31'
+    ser.timeout=0
     #Specify the TimeOut in seconds, so that SerialPort
     #Doesn't hangs
     ser.open()          #Opens SerialPort
     # print port open or closed
     if ser.isOpen():
-        print 'Open: ' + ser.portstr
+        print 'Open: ' + ser.name
 #Function Ends Here
 #Call the Serial Initilization Function, Main Program Starts from here
 init_serial()
 
-temp = raw_input('Type what you want to send, hit enter:\r\n')
-ser.write(temp)         #Writes to the SerialPort
-
-while 1:
-    bytes = ser.readline()  #Read from Serial Port
-    print 'You sent: ' + bytes      #Print What is Read from Port
-    break
+buffer_receive = ser.read(500) #Reads to the SerialPort
+buffer_transmit = ser.write(500) #Writes to the SerialPort
+buffer_receive_convert = map(float, buffer_receive.split(' , '))
+# Splitting the string list into a float list ^^^
+latitude = buffer_receive_convert(0)
+longitude = buffer_receive_convert(1)
+altitude = buffer_receive_convert(2)
 
     #sample code for UART connection
-from machine import UART
-uart = UART(1, 9600)                         # init with given baudrate
-uart.init(9600, bits=8, parity=None, stop=1) # init with given parameters
-while True:
-    data = uart.readall()
-    if data is not None:
-       print(data)
-       break
+#from machine import UART
+#uart = UART(1, 9600)                         # init with given baudrate
+#uart.init(9600, bits=8, parity=None, stop=1) # init with given parameters
+#while True:
+#    data = uart.readall()
+#    if data is not None:
+#       print(data)
+#       break
 #Ctrl+C to Close Python Window
