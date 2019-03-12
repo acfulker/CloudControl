8c48a1d3b058a2a4a279c21042fac966150170f1
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-03-12 17:16:21 -0400

    Update boot.py

--- node/boot.py
+++ node/boot.py
@@ -1,27 +1,25 @@
+from machine import UART
 from network import LoRa
-import socket
+import os
 import time
+import pycom
+import socket
 import ubinascii
 
-# Initialise LoRa in LORAWAN mode.
-# Please pick the region that matches where you are using the device
+#Change default uart from UART(1,9600) to have pass through USB
+drone_uart = UART(0,115200)
+
+#LoRa setup
+#COMMENTED OUT SINCE NO ANTENNA
+pycom.heartbeat(False)     #No heartbeat on boot
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.US915)
-
-# create an OTAA authentication parameters
-app_eui = ubinascii.unhexlify('00250C000100021E')
-app_key = ubinascii.unhexlify('5C6F83079DC0C01B9F77E41B84277C5C')
-
-# join a network using OTAA (Over the Air Activation)
+app_eui = ubinascii.unhexlify('00250C0000010001')
+app_key = ubinascii.unhexlify('5C6F83079DC0C01B9F77E41B84277C5C')     #put the right value here.
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-
-# wait until the module has joined the network
 while not lora.has_joined():
-    time.sleep(0.5)
+    time.sleep(2)
     print('Not yet joined...')
-    
+pycom.heartbeat(True)      #Once joined network, turn on
 print('Joined')
-# create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-
-# set the LoRaWAN data rate
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 4)
