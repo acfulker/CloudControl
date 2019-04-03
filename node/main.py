3406fe3a9241982aa0f68d5a7f0f34ae7342240d
nickmccartney <38221749+nickmccartney@users.noreply.github.com>
2019-04-03 17:21:23 -0400

    Updated main.py
    Functioning implementation of repeated downlink/ uplink

--- node/main.py
+++ node/main.py
@@ -6,65 +6,50 @@ altitude = 0
 #*************************
 #Drone <-> LoPy Connection
 #*************************
-buffer_receive_uart = ""    #Do I need these declared outside of functions to stay in scope?
-buffer_send_lora = []
 
-def receive_uart_data():
+def receiveUart():
+    buffer_receive_uart = None
+    buffer_send_lora = None
     buffer_receive_uart = drone_uart.readall()
-    latitude,longitude,altitude = buffer_receive_uart.split(",")
-    buffer_send_lora = [latitude,longitude,altitude]    #Packetize for send_lora_data
+    #latitude,longitude,altitude = buffer_receive_uart.split(",")                               #FIXME: Functionality for ON drone(figure out)
+    #buffer_send_lora = [latitude,longitude,altitude]    #Packetize for sendLora
+    buffer_send_lora = buffer_receive_uart
+    return buffer_send_lora
 
-def send_uart_data():
+def sendUart(buffer_send_uart):
     drone_uart.write(buffer_send_uart)   #Will this just be a hand off of buffer_send_uart, or an interpretation of the command?
 
 
 #*************************
 #Cloud <-> LoPy Connection
 #*************************
-buffer_receive_lora = ""    #Determine type
-buffer_send_uart = []
-
-def recieve_lora_data():
-    s.setblocking(False)    #Do I need to set this again if send_lora_data --> False
-    buffer_send_uart = s.recv(128)  #Determine size
 
 
-def send_lora_data():
+def recieveLora():
+    buffer_receive_lora = None
+    buffer_send_uart = None
+    s.setblocking(False)    #Do I need to set this again if sendLora --> False
+    buffer_receive_lora = s.recv(128)  #Determine size
+    buffer_send_uart = buffer_receive_lora + "\n"
+    return buffer_send_uart
+
+
+def sendLora(buffer_send_lora):
     s.setblocking(True)    #Block in order to send data
     s.send(buffer_send_lora)    #Need loop with s.send(buffer_send_lora[i])?
     s.setblocking(False)    #Listen by default
-
-
-#*************************
-#Cloud Command Definitions
-#*************************
-
-#I believe this would actually be on the drone
-
 
 #*****************
 #Looping Structure
 #*****************
 
-#IN DEVELOPMENT
-# while True:
-#     i = 0
-#     while i < 5:
-#         receive_uart_data()
-#         i += 1
-#
-#     send_lora_data()
-#
-#     j = 0
-#     while j < 5:
-#         recieve_lora_data()
-#         j += 1
-#
-#     send_uart_data()
+#IN DEVELOPMENT, implement parallel functionality
+while True:
+    buffer_send_lora = receiveUart()
+    if(buffer_send_lora != None):
+        sendLora(buffer_send_lora)
 
-#TEST
-for i in range(100):
-    s.setblocking(True)
-    something = drone_uart.readall()
-    s.send(str(something))
-    time.sleep(1)
+        buffer_send_uart = recieveLora()
+        if(buffer_send_uart != None):
+            sendUart(buffer_send_uart)
+
