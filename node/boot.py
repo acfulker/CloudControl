f3dc17fbd015dce59a6cbc6db1e1bd9da552044f
nickmccartney <38221749+nickmccartney@users.noreply.github.com>
2019-04-03 17:19:13 -0400

    Update boot.py

--- node/boot.py
+++ node/boot.py
@@ -13,7 +13,7 @@ drone_uart = UART(0,115200)
 #COMMENTED OUT SINCE NO ANTENNA
 pycom.heartbeat(False)     #No heartbeat on boot
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.US915)
-app_eui = ubinascii.unhexlify('00250C0000010001')
+app_eui = ubinascii.unhexlify('00250C000100021E')
 app_key = ubinascii.unhexlify('5C6F83079DC0C01B9F77E41B84277C5C')     #put the right value here.
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 while not lora.has_joined():
