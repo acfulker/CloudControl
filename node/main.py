007a8983c0241adc58eeb40a34e27c2c94506740
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-08 15:41:03 -0400

    Update main.py

--- node/main.py
+++ node/main.py
@@ -8,8 +8,8 @@ altitude = 0
 #*************************
 
 def receiveUart():
-    buffer_receive_uart = None
-    buffer_send_lora = None
+    # buffer_receive_uart = None
+    # buffer_send_lora = None
     buffer_receive_uart = drone_uart.readall()
     #latitude,longitude,altitude = buffer_receive_uart.split(",")                               #FIXME: Functionality for ON drone(figure out)
     #buffer_send_lora = [latitude,longitude,altitude]    #Packetize for sendLora
@@ -25,12 +25,12 @@ def sendUart(buffer_send_uart):
 #*************************
 
 
-def recieveLora():
-    buffer_receive_lora = None
-    buffer_send_uart = None
+def receiveLora():
+    # buffer_receive_lora = None
+    # buffer_send_uart = None
     s.setblocking(False)    #Do I need to set this again if sendLora --> False
     buffer_receive_lora = s.recv(128)  #Determine size
-    buffer_send_uart = buffer_receive_lora + "\n"
+    buffer_send_uart = buffer_receive_lora
     return buffer_send_uart
 
 
@@ -43,13 +43,20 @@ def sendLora(buffer_send_lora):
 #Looping Structure
 #*****************
 
-#IN DEVELOPMENT, implement parallel functionality
+#IN DEVELOPMENT
+# while True:
+#     buffer_send_lora = receiveUart()
+#     if(buffer_send_lora != None):
+#         sendLora(buffer_send_lora)
+#
+#         buffer_send_uart = recieveLora()
+#         if(buffer_send_uart != None):
+#             sendUart(buffer_send_uart)
 while True:
+
+    buffer_send_uart = receiveLora()
     buffer_send_lora = receiveUart()
-    if(buffer_send_lora != None):
-        sendLora(buffer_send_lora)
-
-        buffer_send_uart = recieveLora()
-        if(buffer_send_uart != None):
-            sendUart(buffer_send_uart)
-
+    if(buffer_send_uart or buffer_send_lora):
+        sendUart(buffer_send_uart)
+        if(buffer_send_lora != None):
+            sendLora(buffer_send_lora)
