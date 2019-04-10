69670f2c86c9c1ec5ec7c0f4b8f2312eea1ba8ac
nickmccartney <38221749+nickmccartney@users.noreply.github.com>
2019-04-10 17:05:27 -0400

    Update main.py
    Improved readability of the print outs over UART

--- node/main.py
+++ node/main.py
@@ -8,8 +8,8 @@ altitude = 0
 #*************************
 
 def receiveUart():
-    # buffer_receive_uart = None
-    # buffer_send_lora = None
+    buffer_receive_uart = None
+    buffer_send_lora = None
     buffer_receive_uart = drone_uart.readall()
     #latitude,longitude,altitude = buffer_receive_uart.split(",")                               #FIXME: Functionality for ON drone(figure out)
     #buffer_send_lora = [latitude,longitude,altitude]    #Packetize for sendLora
@@ -26,11 +26,11 @@ def sendUart(buffer_send_uart):
 
 
 def receiveLora():
-    # buffer_receive_lora = None
-    # buffer_send_uart = None
+    buffer_receive_lora = None
+    buffer_send_uart = None
     s.setblocking(False)    #Do I need to set this again if sendLora --> False
     buffer_receive_lora = s.recv(128)  #Determine size
-    buffer_send_uart = buffer_receive_lora
+    buffer_send_uart = buffer_receive_lora  #In the case that modifications to the LoRa package are needed
     return buffer_send_uart
 
 
@@ -44,19 +44,12 @@ def sendLora(buffer_send_lora):
 #*****************
 
 #IN DEVELOPMENT
-# while True:
-#     buffer_send_lora = receiveUart()
-#     if(buffer_send_lora != None):
-#         sendLora(buffer_send_lora)
-#
-#         buffer_send_uart = recieveLora()
-#         if(buffer_send_uart != None):
-#             sendUart(buffer_send_uart)
 while True:
+    buffer_send_lora = receiveUart()    #Check for new message from drone
+    if(buffer_send_lora != None):   #If there was data received, send over LoRa
+        sendLora(buffer_send_lora)
 
-    buffer_send_uart = receiveLora()
-    buffer_send_lora = receiveUart()
-    if(buffer_send_uart or buffer_send_lora):
-        sendUart(buffer_send_uart)
-        if(buffer_send_lora != None):
-            sendLora(buffer_send_lora)
+        buffer_send_uart = receiveLora()    #Pull in data from Lora buffer
+        if(str(buffer_send_uart) != "b''"):   #If not empty buffer, send message to drone
+            sendUart(buffer_send_uart)
+            sendUart("\n")
