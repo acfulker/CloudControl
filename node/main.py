68259b1b8a1a040752fb1154583b053c62f3a346
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-18 15:36:23 -0400

    Update main.py

--- node/main.py
+++ node/main.py
@@ -1,65 +1,56 @@
-from network import LoRa
-import socket
-import ubinascii
-import time
-import serial
-#Setting up LoRA connection
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.US915)
-app_eui = ubinascii.unhexlify('00250C0000010001')
-app_key = ubinascii.unhexlify('')#put the right value here.
-while not lora.has_joined():
-    time.sleep(2)
-    print('Not yet joined...')
-print('Joined')
-LoRA_connection = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-LoRA_connection.setsockopt(socket.SOL_LORA, socket.SO_DR, 4)
 #Global Variables
-drone_serialport = 0
+latitude = 0
 longitude = 0
-latitude 0
 altitude = 0
-#Function to Initialize the Serial Port
-def init_serial():
-    drone_serialport = serial.Serial("/dev/cu.usbmodemPy99b4b31")
-    drone_serialport.baudrate= 115200
-    drone_serialport.port= '/dev/cu.usbmodemPy99b4b31'
-    drone_serialport.timeout=0
-    #Specify the TimeOut in seconds, so that SerialPort
-    #Doesn't hangs
-    drone_serialport.open()          #Opens SerialPort
-    # print port open or closed
-    if drone_serialport.isOpen():
-        print 'Open: ' + drone_serialport.name
-#Function Ends Here
-#Call the Serial Initilization Function, Main Program Starts from here
-init_serial()
 
-buffer_receive = drone_serialport.read(500) #Reads to the SerialPort
-buffer_receive_convert = map(float, buffer_receive.split(' , '))
-# Splitting the string list into a float list ^^^
-latitude = buffer_receive_convert(0)
-longitude = buffer_receive_convert(1)
-altitude = buffer_receive_convert(2)
+#*************************
+#Drone <-> LoPy Connection
+#*************************
 
-sending_buffer = [latitude,longitude,altitude] #sending an array of float variables to CloudControl
-LoRA_connection.send(sending_buffer) # sending the buffer of GPS points
+def receiveUart():
+    buffer_receive_uart = None
+    buffer_send_lora = None
+    buffer_receive_uart = drone_uart.readall()
+    #latitude,longitude,altitude = buffer_receive_uart.split(",")                               #FIXME: Functionality for ON drone(figure out)
+    #buffer_send_lora = [latitude,longitude,altitude]    #Packetize for sendLora
+    buffer_send_lora = buffer_receive_uart
+    return buffer_send_lora
 
-#We have to figure out how to keep looking for information and
-#once we see the information break out of it
-received_instructions = LoRA_connection.recv(500)
-drone_serialport.write(received_instructions) #Writes to the SerialPort
+def sendUart(buffer_send_uart):
+    drone_uart.write(buffer_send_uart)   #Will this just be a hand off of buffer_send_uart, or an interpretation of the command?
 
 
+#*************************
+#Cloud <-> LoPy Connection
+#*************************
 
 
-    #sample code for UART connection
-#from machine import UART
-#uart = UART(1, 9600)                         # init with given baudrate
-#uart.init(9600, bits=8, parity=None, stop=1) # init with given parameters
-#while True:
-#    data = uart.readall()
-#    if data is not None:
-#       print(data)
-#       break
-#Ctrl+C to Close Python Window
+def receiveLora():
+    buffer_receive_lora = None
+    buffer_send_uart = None
+    s.setblocking(False)    #Do I need to set this again if sendLora --> False
+    buffer_receive_lora = s.recv(128)  #Determine size
+    buffer_send_uart = buffer_receive_lora  #In the case that modifications to the LoRa package are needed
+    return buffer_send_uart
+
+
+def sendLora(buffer_send_lora):
+    s.setblocking(True)    #Block in order to send data
+    s.send(buffer_send_lora)    #Need loop with s.send(buffer_send_lora[i])?
+    s.setblocking(False)    #Listen by default
+
+#*****************
+#Looping Structure
+#*****************
+
+#IN DEVELOPMENT
+while True:
+    buffer_send_lora = receiveUart()    #Check for new message from drone
+    if(buffer_send_lora != None):   #If there was data received, send over LoRa
+        sendLora(buffer_send_lora)
+
+    buffer_send_uart = receiveLora()    #Pull in data from Lora buffer
+    if(str(buffer_send_uart) != "b''"):   #If not empty buffer, send message to drone
+        sendUart(buffer_send_uart)
+        sendUart("\n")
 
